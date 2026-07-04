ab053184da59ee33b1ce046c5a17c90f64190fff
fallenmeme <pauleneheart86@gmail.com>
2026-07-04 13:00:41 +0800

    Add Intersection Observer for panel visibility

---
+++ script.js
@@ -0,0 +1,21 @@
+const sections = document.querySelectorAll(".panel");
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:0.3
+});
+
+sections.forEach(section=>{
+    observer.observe(section);
+});
